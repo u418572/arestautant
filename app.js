2ec758ee79d9bf8f5787580f1423fa6b6c277ab7
u418572 <u418572@kimo.com>
2023-02-20 15:23:55 +0800

    include and set handlebars

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// include handlebars
+const exphbs = require('express-hnadlebars')
 
+app.engine('handlebars', exphbs({defaultLayout: "main"}))
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
   res.send('texting')
